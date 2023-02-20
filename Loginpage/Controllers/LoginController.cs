9906bd8ad83ab89620c3c7c93868520b0a73e840
Mohammed Fananah <Hp@HP>
2023-02-20 11:36:57 +0200

    Add project files.

---
+++ Loginpage/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loginpage.Models;
+
+namespace Loginpage.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public List<Usermodel1> PutValue()
+        {
+            var users = new List<Usermodel1>
+            {
+                new Usermodel1{Id=1,username="amitkumar",password="abc123"},
+                new Usermodel1{Id=2,username="lisa",password="xyz546"},
+                new Usermodel1{Id=3,username="virat",password="pnpc465"},
+                new Usermodel1{Id=4,username="trina",password="urt324"},
+
+            };
+            return users;
+        }
+        [HttpPost]
+        public IActionResult Verify(Usermodel1 usr)
+        {
+            var u = PutValue();
+            var ue = u.Where(u => u.username.Equals(usr.username));
+            var up = u.Where(u => u.password.Equals(usr.password));
+            if (up.Count() == 1)
+            {
+                ViewBag.message = "Login Success";
+
+                return View("LoginSucess");
+            }
+            else
+            {
+                ViewBag.message = "Login Failed";
+                return View("Login");
+            }
+        }
+    }
+}
---
+++ Loginpage/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Loginpage.Models;
+
+namespace Loginpage.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Loginpage.Models.Usermodel1> Usermodel1 { get; set; }
+    }
+}
